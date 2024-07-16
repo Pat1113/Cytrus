df45d437f40356b05ddd1b18f30bfd9ad0170123
PabloMK7 <hackyglitch2@gmail.com>
2024-07-16 22:00:21 +0200

    Artic Base: Add Artic Controller support (#195)

--- src/android/app/src/main/java/org/cytrus/cytrus_emu/features/settings/model/IntSetting.kt
+++ src/android/app/src/main/java/org/cytrus/cytrus_emu/features/settings/model/IntSetting.kt
@@ -44,7 +44,8 @@ enum class IntSetting(
     TEXTURE_FILTER("texture_filter", Settings.SECTION_RENDERER, 0),
     USE_FRAME_LIMIT("use_frame_limit", Settings.SECTION_RENDERER, 1),
     DELAY_RENDER_THREAD_US("delay_game_render_thread_us", Settings.SECTION_RENDERER, 0),
-    DEVICE_ORIENTATION("default_device_orientation", Settings.SECTION_RENDERER, ActivityInfo.SCREEN_ORIENTATION_USER);
+    DEVICE_ORIENTATION("default_device_orientation", Settings.SECTION_RENDERER, ActivityInfo.SCREEN_ORIENTATION_USER),
+    USE_ARTIC_BASE_CONTROLLER("use_artic_base_controller", Settings.SECTION_CONTROLS, 0);
 
     override var int: Int = defaultValue
 
@@ -72,7 +73,8 @@ enum class IntSetting(
             DEBUG_RENDERER,
             CPU_JIT,
             ASYNC_CUSTOM_LOADING,
-            AUDIO_INPUT_TYPE
+            AUDIO_INPUT_TYPE,
+            USE_ARTIC_BASE_CONTROLLER
         )
 
         fun from(key: String): IntSetting? = IntSetting.values().firstOrNull { it.key == key }
