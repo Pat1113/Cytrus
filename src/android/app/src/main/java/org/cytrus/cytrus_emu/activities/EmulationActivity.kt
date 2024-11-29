PabloMK7/citra

android: implement device rotation options (#182)
* android: implement device rotation options

* fix naming

* move orientation-setting to different section

src/android/app/src/main/java/org/citra/citra_emu/activities/EmulationActivity.kt
@@ -35,6 +35,8 @@ import org.citra.citra_emu.display.ScreenAdjustmentUtil
 import org.citra.citra_emu.features.hotkeys.HotkeyUtility
 import org.citra.citra_emu.features.settings.model.SettingsViewModel
 import org.citra.citra_emu.features.settings.model.view.InputBindingSetting
+import org.citra.citra_emu.features.settings.model.IntSetting
+import org.citra.citra_emu.features.settings.model.Settings
 import org.citra.citra_emu.fragments.MessageDialogFragment
 import org.citra.citra_emu.utils.ControllerMappingHelper
 import org.citra.citra_emu.utils.FileBrowserHelper
@@ -60,7 +62,10 @@ class EmulationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         ThemeUtil.setTheme(this)
 
-        settingsViewModel.settings.loadSettings()
+        isActivityRecreated = savedInstanceState != null
+        if (!isActivityRecreated) {
+            settingsViewModel.settings.loadSettings()
+        }
 
         super.onCreate(savedInstanceState)
 
@@ -73,8 +78,6 @@ class EmulationActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         navController.setGraph(R.navigation.emulation_navigation, intent.extras)
-
-        isActivityRecreated = savedInstanceState != null
 
         // Set these options now so that the SurfaceView the game renders into is the right size.
         enableFullscreenImmersive()
@@ -175,6 +178,11 @@ class EmulationActivity : AppCompatActivity() {
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+
+
+        val orientation = settingsViewModel.settings.getSection(Settings.SECTION_RENDERER)
+            ?.getSetting(IntSetting.DEVICE_ORIENTATION.key) as IntSetting
+        this.requestedOrientation = orientation.int
     }
 
     // Gets button presses

Pat1113/Cytrus

src/android/app/src/main/java/org/cytrus/cytrus_emu/activities/EmulationActivity.kt
@@ -25,7 +25,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.preference.PreferenceManager
-import org.cytrus.cytrus_emu.CytrusApplication
+import org.cytrus.cytrus_emu.CitraApplication
 import org.cytrus.cytrus_emu.NativeLibrary
 import org.cytrus.cytrus_emu.R
 import org.cytrus.cytrus_emu.camera.StillImageCameraHelper.OnFilePickerResult
@@ -35,6 +35,8 @@ import org.cytrus.cytrus_emu.display.ScreenAdjustmentUtil
 import org.cytrus.cytrus_emu.features.hotkeys.HotkeyUtility
 import org.cytrus.cytrus_emu.features.settings.model.SettingsViewModel
 import org.cytrus.cytrus_emu.features.settings.model.view.InputBindingSetting
+import org.cytrus.cytrus_emu.features.settings.model.IntSetting
+import org.cytrus.cytrus_emu.features.settings.model.Settings
 import org.cytrus.cytrus_emu.fragments.MessageDialogFragment
 import org.cytrus.cytrus_emu.utils.ControllerMappingHelper
 import org.cytrus.cytrus_emu.utils.FileBrowserHelper
@@ -60,7 +62,10 @@ class EmulationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         ThemeUtil.setTheme(this)
 
-        settingsViewModel.settings.loadSettings()
+        isActivityRecreated = savedInstanceState != null
+        if (!isActivityRecreated) {
+            settingsViewModel.settings.loadSettings()
+        }
 
         super.onCreate(savedInstanceState)
 
@@ -73,8 +78,6 @@ class EmulationActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         navController.setGraph(R.navigation.emulation_navigation, intent.extras)
-
-        isActivityRecreated = savedInstanceState != null
 
         // Set these options now so that the SurfaceView the game renders into is the right size.
         enableFullscreenImmersive()
@@ -175,6 +178,11 @@ class EmulationActivity : AppCompatActivity() {
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+
+
+        val orientation = settingsViewModel.settings.getSection(Settings.SECTION_RENDERER)
+            ?.getSetting(IntSetting.DEVICE_ORIENTATION.key) as IntSetting
+        this.requestedOrientation = orientation.int
     }
 
     // Gets button presses