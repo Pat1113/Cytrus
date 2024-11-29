PabloMK7/citra

android: add quicksave hotkeys (#181)

src/android/app/src/main/java/org/citra/citra_emu/features/hotkeys/HotkeyUtility.kt
@@ -4,10 +4,14 @@
 
 package org.citra.citra_emu.features.hotkeys
 
+import android.content.Context
+import android.widget.Toast
+import org.citra.citra_emu.NativeLibrary
+import org.citra.citra_emu.R
 import org.citra.citra_emu.utils.EmulationLifecycleUtil
 import org.citra.citra_emu.display.ScreenAdjustmentUtil
 
-class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil) {
+class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil, private val context: Context) {
 
     val hotkeyButtons = Hotkey.entries.map { it.button }
 
@@ -18,6 +22,23 @@ class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil) {
                 Hotkey.CYCLE_LAYOUT.button -> screenAdjustmentUtil.cycleLayouts()
                 Hotkey.CLOSE_GAME.button -> EmulationLifecycleUtil.closeGame()
                 Hotkey.PAUSE_OR_RESUME.button -> EmulationLifecycleUtil.pauseOrResume()
+                Hotkey.QUICKSAVE.button -> {
+                    NativeLibrary.saveState(NativeLibrary.QUICKSAVE_SLOT)
+                    Toast.makeText(context,
+                        context.getString(R.string.quicksave_saving),
+                        Toast.LENGTH_SHORT).show()
+                }
+                Hotkey.QUICKLOAD.button -> {
+                    val wasLoaded = NativeLibrary.loadStateIfAvailable(NativeLibrary.QUICKSAVE_SLOT)
+                    val stringRes = if(wasLoaded) {
+                        R.string.quickload_loading
+                    } else {
+                        R.string.quickload_not_found
+                    }
+                    Toast.makeText(context,
+                        context.getString(stringRes),
+                        Toast.LENGTH_SHORT).show()
+                }
                 else -> {}
             }
             return true

Pat1113/Cytrus

src/android/app/src/main/java/org/cytrus/cytrus_emu/features/hotkeys/HotkeyUtility.kt
@@ -4,10 +4,14 @@
 
 package org.cytrus.cytrus_emu.features.hotkeys
 
+import android.content.Context
+import android.widget.Toast
+import org.cytrus.cytrus_emu.NativeLibrary
+import org.cytrus.cytrus_emu.R
 import org.cytrus.cytrus_emu.utils.EmulationLifecycleUtil
 import org.cytrus.cytrus_emu.display.ScreenAdjustmentUtil
 
-class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil) {
+class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil, private val context: Context) {
 
     val hotkeyButtons = Hotkey.entries.map { it.button }
 
@@ -18,6 +22,23 @@ class HotkeyUtility(private val screenAdjustmentUtil: ScreenAdjustmentUtil) {
                 Hotkey.CYCLE_LAYOUT.button -> screenAdjustmentUtil.cycleLayouts()
                 Hotkey.CLOSE_GAME.button -> EmulationLifecycleUtil.closeGame()
                 Hotkey.PAUSE_OR_RESUME.button -> EmulationLifecycleUtil.pauseOrResume()
+                Hotkey.QUICKSAVE.button -> {
+                    NativeLibrary.saveState(NativeLibrary.QUICKSAVE_SLOT)
+                    Toast.makeText(context,
+                        context.getString(R.string.quicksave_saving),
+                        Toast.LENGTH_SHORT).show()
+                }
+                Hotkey.QUICKLOAD.button -> {
+                    val wasLoaded = NativeLibrary.loadStateIfAvailable(NativeLibrary.QUICKSAVE_SLOT)
+                    val stringRes = if(wasLoaded) {
+                        R.string.quickload_loading
+                    } else {
+                        R.string.quickload_not_found
+                    }
+                    Toast.makeText(context,
+                        context.getString(stringRes),
+                        Toast.LENGTH_SHORT).show()
+                }
                 else -> {}
             }
             return true